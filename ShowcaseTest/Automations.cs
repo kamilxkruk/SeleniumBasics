17626811d52c36ff27e412d490cb6fac4f8fd2cf
kamilxkruk <39237618+kamilxkruk@users.noreply.github.com>
2020-02-22 14:01:01 +0100

    Add files via upload

---
+++ ShowcaseTest/Automations.cs
@@ -0,0 +1,42 @@
+﻿using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShowcaseTest
+{
+    public class Automations
+    {
+        #region Const Xpaths
+        private const string googleInputXpath = @"//*[@id='tsf']/div[2]/div[1]/div[1]/div/div[2]/input";
+        private const string googleSearchButtonXpath = @"//*[@id='tsf']/div[2]/div[1]/div[3]/center/input[1]";
+
+        private const string goToCitySearchButtonXpath = @"//*[@id='container']/starting-screen/div/div/div[2]/div[2]/ng-transclude/desktop-starting-screen/div/button";
+        
+        #endregion
+
+        public void AutomateJakDojade(IWebDriver webDriver)
+        {
+            webDriver.Navigate().GoToUrl(@"https:\\jakdojade.pl\lista-miast");
+
+            var goToCitySearchButton = webDriver.FindElements(By.XPath(goToCitySearchButtonXpath)).FirstOrDefault();
+            goToCitySearchButton.Click();
+
+            var cities = webDriver.FindElements(By.XPath(@"//*[@id='citiesContainer']/li"));
+            var cityNames = cities.Select(c => c.Text);
+            var katowice = cities.Where(c => c.Text.Contains("Katowice")).FirstOrDefault();
+            katowice.Click();
+        }
+
+        public void AutomateGoogle(IWebDriver webDriver, string searchPhrase)
+        {
+            webDriver.Navigate().GoToUrl("https:\\google.com");
+            var googleInput = webDriver.FindElements(By.XPath(googleInputXpath)).FirstOrDefault();
+            var googleSearchButton = webDriver.FindElements(By.XPath(googleSearchButtonXpath)).FirstOrDefault();
+
+            googleInput.SendKeys("Selenium" + Keys.Escape + Keys.Enter);
+        }
+    }
+}
---
+++ ShowcaseTest/Program.cs
@@ -0,0 +1,41 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShowcaseTest
+{
+    class Program
+    {
+       
+
+        static void Main(string[] args)
+        {
+            IWebDriver driver = null;
+
+
+            try
+            {
+                driver = new ChromeDriver();
+                driver.Manage().Window.Maximize();
+
+                new Automations().AutomateJakDojade(driver);
+
+                Console.ReadKey();
+            }
+            finally
+            {
+                if (driver != null)
+                {
+                    driver.Quit();
+                }
+            }
+          
+
+        }
+    }
+
+}
